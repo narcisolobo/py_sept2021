71432f3f38cf64128eca5d543d65df10ecf57a45
Narciso Lobo <narcisolobo@gmail.com>
2021-09-01 10:57:08 -0500

    first commit

---
+++ variables.js
@@ -0,0 +1,42 @@
+// Variables and Datatypes
+// Naming conventions for JS Variables
+// Camel Case
+
+// Naming rules for JS Variables
+// Cannot start with a number
+// Cannot have any hyphens/dashes
+// Cannot be a keyword already
+
+var myNickname = "Narciso \"Ciso\" Lobo";
+
+// Primitive Datatypes - Strings, Numbers, Booleans, null, undefined
+// Strings - strings are "strings" of characters
+
+console.log(typeof "Nichole")
+
+// Concatenation of strings
+var firstName = "Nichole"
+var lastName = "King"
+
+var fullName = firstName + " " + lastName;
+
+// Numbers
+// JS makes no distinction between floating point numbers and integers
+
+100 // --> number
+13.45 // --> also a number
+
+var age = 32;
+var num1 = 10
+var num2 = "10"
+
+// Booleans
+// True or False
+
+var isSleeping = false
+
+// Null - nothing but intentional
+var choice = null;
+
+// Undefined
+var choice;
